3b88b5aba19666c713c2de9b77739558e6348e15
Sujan <sujaan1919@gmail.com>
2026-04-25 16:36:12 -0500

    Fix Feed layout breaking and iOS music not loading by anchoring the invisible YouTube iframe to the center of the fixed viewport

--- src/pages/Feed.tsx
+++ src/pages/Feed.tsx
@@ -1139,7 +1139,7 @@ const Feed = () => {
 
 
       {/* Global Shared YouTube Player (Fast Audio Switching) */}
-      <div className="absolute opacity-0 pointer-events-none z-0">
+      <div className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 opacity-0 pointer-events-none z-[-1]">
         <YouTube 
           videoId="" // Video loaded dynamically via ytPlayer.loadVideoById
           opts={{ 
